185f48645b261f74138efd7994f10e85d6c597c6
Kemboijames7 <jameskemich@gmail.com>
2024-05-30 23:15:14 +0300

    new-exercises

---
+++ Exercism/js/main1.js
@@ -0,0 +1,9 @@
+const convert = (num) => {
+  const answer = [];
+   
+   if (num % 3 === 0) answer.push('Pling');
+   if (num % 5 === 0) answer.push('Plang');
+   if (num % 7 === 0) answer.push('Plong');
+   
+   return answer.length ? answer.join('') : num.toString();
+};
